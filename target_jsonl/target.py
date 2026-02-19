05668c965f334020db85d92bd99b1c779720bf5e
Reuben Frankel <rfrankel@matatika.com>
2026-02-19 00:21:07 +0000

    Define output directory setting

--- target_jsonl/target.py
+++ target_jsonl/target.py
@@ -17,24 +17,11 @@ class TargetJSONL(Target):
 
     config_jsonschema = th.PropertiesList(
         th.Property(
-            "filepath",
+            "output_dir",
             th.StringType(nullable=False),
-            title="Output File Path",
-            description="The path to the target output file",
-        ),
-        th.Property(
-            "file_naming_scheme",
-            th.StringType(nullable=False),
-            title="File Naming Scheme",
-            description="The scheme with which output files will be named",
-        ),
-        th.Property(
-            "auth_token",
-            th.StringType(nullable=False),
-            secret=True,  # Flag config as protected.
-            required=True,
-            title="Auth Token",
-            description="The path to the target output file",
+            title="Output Directory",
+            description="Path to the target output directory",
+            default="output",
         ),
     ).to_dict()
 
